0b96d7416c1095eb1d4ab01f30aa778e75374337
Pranay <pranayjalan.work@gmail.com>
2024-09-24 20:59:21 +0530

    updating functions

--- app/contractRefs.js
+++ app/contractRefs.js
@@ -1,2 +1,99 @@
-export const contractAddress = "";
-export const contractAbi = [];
+export const contractAddress = "0x57aDE1D14B2e6bfF5B0514B841Ae9E56D50F778D";
+export const contractAbi = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_favNumber",
+        type: "uint256",
+      },
+    ],
+    name: "createUser",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "deleteUser",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_favNumber",
+        type: "uint256",
+      },
+    ],
+    name: "updateUser",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_userId",
+        type: "uint256",
+      },
+    ],
+    name: "getUser",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getUserCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
--- app/utils.js
+++ app/utils.js
@@ -6,44 +6,67 @@ export let signer = null;
 export let provider;
 
 export async function connectWithMetamask() {
-    console.log("calling connect with metamask");
-    if (window.ethereum == null) {
-        console.log("MetaMask not installed; using read-only defaults");
-        provider = ethers.getDefaultProvider();
-        console.log(provider);
-    } else {
-        provider = await new ethers.BrowserProvider(window.ethereum);
-        console.log(provider);
-        signer = await provider.getSigner();
-        console.log(signer);
-        return await provider.send("eth_requestAccounts", [0]);
-    }
+  console.log("calling connect with metamask");
+  if (window.ethereum == null) {
+    console.log("MetaMask not installed; using read-only defaults");
+    provider = ethers.getDefaultProvider();
+    console.log(provider);
+  } else {
+    provider = await new ethers.BrowserProvider(window.ethereum);
+    console.log(provider);
+    signer = await provider.getSigner();
+    console.log(signer);
+    return await provider.send("eth_requestAccounts", [0]);
+  }
 }
 connectWithMetamask();
 
 // Utility to create a new user
-export async function createUser() {
-
+export async function createUser(name, favNumber) {
+  await connectWithMetamask();
+  const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+  const tx = await contract.createUser(name, favNumber);
+  console.log(tx);
+  return tx;
 }
 
 // Utility to get a user's details
-export async function getUser() {
-
+export async function getUser(userId) {
+  await connectWithMetamask();
+  const contract = new ethers.Contract(contractAddress, contractAbi, provider);
+  const user = await contract.getUser(userId);
+  console.log("user details", user);
+  return {
+    id: user[0].toString(),
+    name: user[1],
+    favNumber: user[2].toString(),
+  };
 }
 
 // Utility to delete a user
-export async function deleteUser() {
-
+export async function deleteUser(userId) {
+  await connectWithMetamask();
+  const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+  const tx = await contract.deleteUser(userId);
+  console.log("delete", tx);
+  return tx;
 }
 
 // Utility to update a user's name and favorite number
-export async function updateUser() {
-
+export async function updateUser(userId, name, favNumber) {
+  await connectWithMetamask();
+  const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+  const tx = await contract.updateUser(userId, name, favNumber);
+  console.log("update", tx);
+  return tx;
 }
 
-
-
 // Utility to get the total number of users
-export async function getUserCount() {
 
+export async function getUserCount() {
+  await connectWithMetamask();
+  const contract = new ethers.Contract(contractAddress, contractAbi, provider);
+  const count = await contract.getUserCount();
+  console.log("Total number of users:", count.toString());
+  return count.toString();
 }
